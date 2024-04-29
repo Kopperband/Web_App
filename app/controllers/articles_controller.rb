714bcb905f81af5c8495f700f4c3a31707ed716a
Kopperband <nathan.hansford@me.com>
2024-04-29 16:43:02 -0300

    First day of using multiple controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @articles  = Article.new
+
+  end
+
+  def create
+    @articles = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @articles
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
