57a4dcc89bd4c610b0de5032475da96ff3417e1d
Nathan Hansford <nathan.hansford@me.com>
2024-04-30 09:33:27 -0300

    Changing all local variables from @articles to @article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,13 +13,40 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @articles = Article.new(title: "...", body: "...")
+    @articles = Article.new(article_params)
 
-    if @article.save
-      redirect_to @articles
+    if @articles.save
+      redirect_to articles_path, notice: "Article Saved!"
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @articles = Article.find(params[:id])
+  end
+
+  def update
+    @articles = Article.find(params[:id])
+
+    if @articles.update(article_params)
+      redirect_to articles_path, notice: "Article Changed!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+
+    redirect_to articles_path, notice: "Article Deleted"
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 
 end
